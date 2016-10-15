055d7e3cd8d6d9695ea49cca998f40a9ed996be1
cooshko <coosh@qq.com>
2016-10-15 16:38:26 +0800

    三级菜单作业

--- day01/三层菜单/main.py
+++ day01/三层菜单/main.py
@@ -7,19 +7,46 @@ import pickle, sys
 with open('data.pickle', 'rb') as f:
     ds = pickle.loads(f.read())
 
-def menu(ds):
-    # 菜单最底层的数据是集合类型，当ds不是集合，则代表还在菜单当中
-    if not isinstance(ds, set):
-        # 循环打印当前层的菜单，除非返回或退出
-        # 生成一个列表，每个列表值都是ds字典的
-        current_menu = dict()
-        for i in len(ds):
-            current_menu[i+1] = ds[i]
-        while True:
-            for i in range(len(current_menu)):
-                print('%d. %s' % (i, current_menu))
 
+def menu(ds):
+        # 循环打印当前层的菜单，除非返回或退出
+
+        # 生成一个字典，序号:当前层的索引
+        current_menu = dict()
+        id_num = 1
+        for key in ds:
+            # 序号要转换成字符串，方便以后调用
+            current_menu[str(id_num)] = key
+            id_num += 1
+
+        # 循环打印菜单
+        while True:
+            print('=' * 30)
+            # 以序号顺序来打印字典（当前菜单层）
+            for k in sorted(current_menu.keys()):
+                print('%s. %s' % (k, current_menu[k]))
+            print('')
+            print('(b)返回上一层')
+            print('(q)退出')
+            pick = input('请选择: ')
+            # 用户输入有误
+            if pick not in current_menu.keys() and pick not in ['b', 'q']:
+                print('找不到你输入的选项，请重新输入')
+            elif pick == 'b':
+                # 返回上一层菜单
+                return
+            elif pick == 'q':
+                print('正在退出程序')
+                sys.exit()
+            else:
+                # 打印用户的选项
+                print('你选择了 ', current_menu[pick])
+                # 假如当前层仍是字典类型，代表仍有下级内容，则再次递归调用menu函数
+                if isinstance(ds, dict):
+                    menu(ds[current_menu[pick]])
+                else:
+                    print('已是最后一层菜单')
 
 
 if __name__ == '__main__':
-    print(ds)
+    menu(ds)

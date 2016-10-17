649ff3677477ff2f34b14d9a1f1263b0c15b1dcf
cooshko <coosh@qq.com>
2016-10-17 12:20:13 +0800

    购物车

--- day02/main.py
+++ day02/main.py
@@ -55,36 +55,41 @@ def shopping_menu(user: dict, ds):
         else:
             raise Exception('ds type wrong')
         print('''
-(c)购物车
-(q)退出
-(b)返回''')
+(c)购物车  (r)购买记录  (q)退出  (b)返回''')
         user_choice = input('请选择：').strip()
         # 用户输入的是数字
         if user_choice.isdigit():
             user_choice = int(user_choice)
-            selected_item = menu_list[user_choice]
-            if product_flag:
-                while True:
-                    p_num = input('你选择的是 %s，请输入件数：' % selected_item[0])
-                    if p_num.isdigit():
-                        p_num = int(p_num)
-                        p_name = selected_item[0]
-                        p_price = int(selected_item[1])
-                        cart_update('add', p_name, p_price, p_num)
-                        print('已加入购物车：%s x %d' % (p_name, p_num))
-                        break
+            if user_choice < len(menu_list):
+                selected_item = menu_list[user_choice]
+                if product_flag:
+                    while True:
+                        p_num = input('你选择的是 %s，请输入件数：' % selected_item[0])
+                        if p_num.isdigit():
+                            p_num = int(p_num)
+                            p_name = selected_item[0]
+                            p_price = int(selected_item[1])
+                            cart_update('add', p_name, p_price, p_num)
+                            print('已加入购物车：%s x %d' % (p_name, p_num))
+                            break
+                else:
+                    shopping_menu(user, ds[selected_item])
             else:
-                shopping_menu(user, ds[selected_item])
+                print('你的输入超出范围，请重新选择')
+                input()
         # 如果用户输入的不是数字
-        elif user_choice in ['q', 'b', 'c']:
+        elif user_choice in ['q', 'b', 'c', 'r']:
             if user_choice == 'q':
                 exit()
             elif user_choice == 'b':
                 return
             elif user_choice == 'c':
                 cart_display()
+            elif user_choice == 'r':
+                log_display()
         else:
             print('你的输入有误，请重新选择')
+            input()
 
 
 # 充值函数
@@ -111,7 +116,21 @@ def user_recharge_money():
 
 # 用户支付
 def user_pay(total_amount: int):
-    pass
+    # 扣钱
+    CURRENT_USER['wallet'] -= total_amount
+    # 写入日志
+    dt = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    # 每一个购物车商品记录为一行日志
+    for p_name, p in CURRENT_USER['cart'].items():
+        # 记录的内容为购买日期时间、商品名称、数量、单项商品总金额
+        log_msg = "%s %s %d %d %d" % (dt, p_name, p[0], p[1], p[0]*p[1])
+        CURRENT_USER['log'].append(log_msg)
+    # 清空购物车
+    CURRENT_USER['cart'] = dict()
+    # 保存用户状态
+    save_user_info(CURRENT_USER)
+    print('谢谢，您总共消费%d元，点击任意键返回' % total_amount)
+    input()
 
 
 # 展示购物车函数
@@ -119,12 +138,17 @@ def cart_display():
     while True:
         # 分割线
         print(sep_row)
+        print('您的购物车内有如下物品\n')
         # 如果当前用户购物车中有商品
         if CURRENT_USER['cart']:
             # 结算总金额
             total_amount = 0
+            # 商品种类数量
+            kind_num = len(CURRENT_USER['cart'])
+            # 商品名称列表
+            p_list = enumerate(sorted(CURRENT_USER['cart'].keys()))
             # 对购物车的商品名称进行排序，然后枚举并打印序号和商品明细
-            for p_id, p_name in enumerate(sorted(CURRENT_USER['cart'].keys())):
+            for p_id, p_name in p_list:
                 p_price = CURRENT_USER['cart'][p_name][0]
                 p_num = CURRENT_USER['cart'][p_name][1]
                 p_amount = p_price * p_num
@@ -133,27 +157,47 @@ def cart_display():
             print('总金额: %d' % total_amount)
             # 如果用户有足够的钱支付，则打印结算选项
             if CURRENT_USER['wallet'] - total_amount >= 0:
-                user_action = input('(p)付款  (u)编辑购物车  请选择：')
+                user_action = input('(p)付款  (u)编辑购物车 (b)返回  请选择：')
                 payable_flag = True
             else:
-                user_action = input('您的余额不足，(r)充值  (u)编辑购物车  请选择：')
+                user_action = input('您的余额不足，(r)充值  (u)编辑购物车 (b)返回  请选择：')
                 payable_flag = False
             # 判断用户选择
             if payable_flag and user_action == 'p':
                 # 付款
-                pass
+                user_pay(total_amount)
+                return True
             elif not payable_flag and user_action == 'r':
                 # 充值
                 user_recharge_money()
+                continue
+            elif user_action == 'b':
+                return
             elif user_action == 'u':
                 # 更新购物车
-                pass
-            else:
-                print('您的输入有误，请重新输入')
-                continue
+                user_choice = input('请输入商品编号：')
+                if user_choice.isdigit():
+                    user_choice = int(user_choice)
+                    # 用户输入正确
+                    if user_choice < kind_num:
+                        p_name = sorted(CURRENT_USER['cart'].keys())[user_choice]
+                        p_price = CURRENT_USER['cart'][p_name][0]
+                        p_num = CURRENT_USER['cart'][p_name][1]
+                        print('您选择的是 %s ，购物车中有%d件' % (p_name, p_num))
+                        adjust_offset = input('请输入加减件数（比如 +1 或者 -1）：').strip()
+                        try:
+                            adjust_offset = int(adjust_offset)
+                            if adjust_offset > 0:
+                                cart_update('add', p_name, p_price, adjust_offset)
+                            elif adjust_offset < 0:
+                                cart_update('minus', p_name, p_price, adjust_offset)
+                            continue
+                        except:
+                            pass
+            print('您的输入有误，请重新输入')
         # 购物车中没有商品，任意键返回
         else:
-            print('你的购物车空空如也\n\n(b)返回')
+            print('空空如也\n\n(b)返回')
             input()
             return
 
@@ -162,20 +206,20 @@ def cart_display():
 def cart_update(action, p_name, p_price, p_num):
     # 检查购物车中是否已有该商品，如果没有，则创建一个且件数为0
     CURRENT_USER['cart'][p_name] = CURRENT_USER['cart'].get(p_name, [p_price, 0])
-    # 往购物车加入商品
-    if action == 'add':
-        CURRENT_USER['cart'][p_name][1] += p_num
-    # 减少购物车中的商品数量
-    elif action == 'minus':
-        CURRENT_USER['cart'][p_name][1] -= p_num
-        # 如果扣减后件数少于等于0，则删除该商品在购物车的记录
-        if CURRENT_USER['cart'][p_name][1] <= 0:
-            CURRENT_USER['cart'].pop(p_name)
-    else:
-        pass
-    # 每次更新完购物车，都需要保存到文件中去，即使程序退出了，下次再次登陆，也能恢复购物车记录
+    CURRENT_USER['cart'][p_name][1] += p_num
+    # 如果扣减后件数少于等于0，则删除该商品在购物车的记录
+    if CURRENT_USER['cart'][p_name][1] <= 0:
+        CURRENT_USER['cart'].pop(p_name)
     save_user_info(CURRENT_USER)
 
+
+# 显示购买日志
+def log_display():
+    print(sep_row)
+    print('您的购买记录如下：\n\n%-15s %-2s %-2s %-2s %-2s' % ('购买时间', '商品', '单价', '件数', '金额'))
+    for log_msg in CURRENT_USER['log']:
+        print(log_msg)
+    input('任意键返回')
 
 # 程序入口
 if __name__ == '__main__':
@@ -206,8 +250,7 @@ if __name__ == '__main__':
 1. 购物
 2. 充值
 
-(q) 退出
-请选择：''')
+(q)退出  请选择：''')
         # 用户输入q，则退出程序
         if entry_choice == 'q':
             exit()

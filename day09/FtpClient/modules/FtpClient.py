fcfe9593c2f4388fe3fbe6a66925e9e1c1537d4a
cooshko <coosh@qq.com>
2016-11-03 16:57:26 +0800

    FTP

--- day09/FtpClient/modules/FtpClient.py
+++ day09/FtpClient/modules/FtpClient.py
@@ -3,7 +3,7 @@
 # @Author  : Coosh
 # @File    : FtpClient.py
 
-import socket, hashlib
+import socket, hashlib, os, sys
 
 
 class FtpClient(object):
@@ -29,7 +29,106 @@ class FtpClient(object):
 
     def start(self):
         if self.connect():
-            pass
+            self.ui()
+
+    def ui(self):
+        while True:
+            cmd_str = input(">> ").strip()
+            if not cmd_str:
+                continue
+            cmd = cmd_str.split(maxsplit=1)[0]
+            args = cmd_str.replace(cmd, "").lstrip()
+            if cmd == 'get':
+                self.client_get(args)
+            elif cmd == 'put':
+                self.client_put(args)
+            else:
+                self.sock.send(cmd_str.encode())
+                feedback = self.recv_once()
+                status, msg = feedback.decode().split("|", maxsplit=1)
+                print(msg)
+
+    def client_get(self, args):
+        filename = args
+        tmp_file = args + '.tmp'
+        from_position = 0
+        if os.path.isfile(tmp_file):
+            # 断点续传
+            from_position = os.path.getsize(tmp_file)
+        cmd_str = r'get "{filename:s}" from {position:d}'.format(filename=filename, position=from_position)
+        self.sock.send(cmd_str.encode())    # 真正发送命令
+        feedback = self.sock.recv(1024).decode()
+        status, arg = feedback.split("|")
+        # 接收服务器的确认消息，里面包括确认和大小或者拒绝和原因。如果OK则通知服务器开始发送数据，如果拒绝则打印原因
+        if status == 'OKAY':
+            total_size = int(arg)
+            received_size = 0
+            m = hashlib.md5()
+            self.sock.send(b"START")
+            with open(tmp_file, 'ab') as fh:
+                while total_size - received_size > 1024:
+                    data = self.sock.recv(1024)
+                    received_size += 1024
+                    fh.write(data)
+                    m.update(data)
+                    self.process_bar(received_size, total_size)
+                else:
+                    data = self.sock.recv(total_size - received_size)
+                    received_size += len(data)
+                    fh.write(data)
+                    m.update(data)
+                    self.process_bar(received_size, total_size)
+            d = self.sock.recv(1024)
+            d = self.sock.recv(1024)
+            d = self.sock.recv(1024)
+            md5_value = d.decode()
+            print()
+            print("md5 from server: ", md5_value)
+            print("md5 from local : ", m.hexdigest())
+        elif status == 'NO':
+            print(arg)
+
+    def process_bar(self, m, n):
+        """
+        进度条
+        :param m:
+        :param n:
+        :return:
+        """
+        percent = int(m / n * 100)
+        arrow = "=" * int(percent/10) + ">"
+        msg = "\r" + arrow + " %d%%" % percent
+        sys.stdout.write(msg)
+        sys.stdout.flush()
+
+    def client_put(self, args):
+        pass
+
+    def send_once(self, bytes_data):
+        if not isinstance(bytes_data, bytes):
+            raise Exception("发送的数据必须为bytes类型")
+        data_size = len(bytes_data)
+        if data_size > 0:
+            m = hashlib.md5()
+            m.update(bytes_data)
+            md5_value = m.hexdigest()
+            msg = md5_value + "|" + str(data_size)
+            self.sock.send(msg.encode())
+            self.sock.recv(1024)    # 接收开始信号
+            self.sock.send(bytes_data)
+
+    def recv_once(self):
+        msg = self.sock.recv(1024)
+        md5_value, size_str = msg.decode().split("|", maxsplit=1)
+        msg_size = int(size_str)
+        recv_data = b''
+        self.sock.send(b'START')    # 发送开始信号
+        while msg_size - len(recv_data) > 1024:
+            recv_data += self.sock.recv(1024)
+        else:
+            recv_data += self.sock.recv(msg_size - len(recv_data))
+        return recv_data
+
 
 if __name__ == '__main__':
     myclient = FtpClient()
--- day09/FtpServer/modules/FtpServer.py
+++ day09/FtpServer/modules/FtpServer.py
@@ -3,7 +3,7 @@
 # @Author  : Coosh
 # @File    : ftpserver.py
 
-import os, sys, re, socketserver, logging
+import os, sys, re, socketserver, logging, hashlib
 
 
 class FtpServer(socketserver.BaseRequestHandler):
@@ -28,18 +28,21 @@ class FtpServer(socketserver.BaseRequestHandler):
                 continue
             bdata = conn.recv(1024)     # 接收客户端的指令消息
             if len(bdata) == 0:
-                self.__log("{client_ip:s}已经断开".format(client_ip=self.client_address[0]))
+                self.__log("{username:s}已经断开".format(username=self.current_user))
                 break
             cmd_str = bdata.decode().strip()
             cmd = cmd_str.split(maxsplit=1)[0]
             args = cmd_str.replace(cmd, "").lstrip()
-            if hasattr(self, cmd):
-                func = getattr(self, cmd)
+            if hasattr(self, "cmd_" + cmd):
+                func = getattr(self, "cmd_" + cmd)
                 func(args)
+            else:
+                self.send_once("NO|该指令不存在".encode())
 
     def setup(self):
-        connect_msg = "{client_ip:s}已连接".format(client_ip=self.client_address[0])
-        self.__log(connect_msg)
+        # connect_msg = "{client_ip:s}已连接".format(client_ip=self.client_address[0])
+        # self.__log(connect_msg)
+        pass
 
     def __log(self, msg, level=logging.INFO):
         """
@@ -67,6 +70,7 @@ class FtpServer(socketserver.BaseRequestHandler):
         logger.addHandler(fh)
         logger.addHandler(ch)
         # 调用
+        msg = "{ip:s}:{port:d} ".format(ip=self.client_address[0], port=self.client_address[1]) + msg
         logger.info(msg)
 
     def __login(self):
@@ -91,6 +95,7 @@ class FtpServer(socketserver.BaseRequestHandler):
                     self.current_user = username
                     self.user_quota = int(user_quota)
                     self.home = os.path.join(FtpServer.ROOT_DIR, 'home', self.current_user)
+                    self.__log(username + "登陆成功")
                     self.request.send("OKAY|欢迎".encode())
                     return True
                 else:
@@ -109,7 +114,19 @@ class FtpServer(socketserver.BaseRequestHandler):
         :param p:目标绝对路径
         :return: bool
         """
-        pass
+        p = os.path.abspath(p)
+        return self.home in p
+
+    def __used_quota(self):
+        """
+        获取当前用户已使用的配额
+        :return: int
+        """
+        used_quota = 0
+        for root, dirname, filenames in os.walk(self.home):
+            for filename in filenames:
+                used_quota += os.path.getsize(os.path.join(root, filename))
+        return used_quota
 
     def __check_quota(self):
         """
@@ -118,35 +135,134 @@ class FtpServer(socketserver.BaseRequestHandler):
         """
         pass
 
-    def ls(self):
+    def check_access(self, p):
         """
-        浏览当前目录
+
+        :param p: 目标路径
         :return:
         """
-        pass
+        full_path = os.path.abspath(os.path.join(self.home, self.pwd, p.strip()))
+        if self.__accessable(full_path):
+            return full_path
+        else:
+            self.send_once("NO|只允许访问你拥有目录".encode())
+            return False
 
-    def mkdir(self, args):
+    def send_once(self, bytes_data):
+        if not isinstance(bytes_data, bytes):
+            raise Exception("发送的数据必须为bytes类型")
+        data_size = len(bytes_data)
+        if data_size > 0:
+            m = hashlib.md5()
+            m.update(bytes_data)
+            md5_value = m.hexdigest()
+            msg = md5_value + "|" + str(data_size)
+            self.request.send(msg.encode())
+            self.request.recv(1024)  # 接收开始信号
+            self.request.send(bytes_data)
+
+    def recv_once(self):
+        msg = self.request.recv(1024)
+        md5_value, size_str = msg.decode().split("|", maxsplit=1)
+        msg_size = int(size_str)
+        recv_data = b''
+        self.request.send(b'START')  # 发送开始信号
+        while msg_size - len(recv_data) > 1024:
+            recv_data += self.request.recv(1024)
+        else:
+            recv_data += self.request.recv(msg_size - len(recv_data))
+        return recv_data
+
+    def cmd_cd(self, args):
+        """
+        切换目录
+        :return:
+        """
+        full_path = self.check_access(args)
+        if not full_path:
+            return False
+        self.pwd = full_path.replace(self.home, "").lstrip(os.sep)
+        if len(self.pwd) == 0:
+            msg = "OKAY|你正在访问家目录"
+        else:
+            msg = "OKAY|你正在访问" + self.pwd
+        self.send_once(msg.encode())
+
+    def cmd_ls(self, args):
+        """
+        浏览目录
+        :return:
+        """
+        full_path = self.check_access(args)
+        if not full_path:
+            return False
+        dir_list = os.listdir(full_path)
+        ret = '.\n..\n'
+        for item in dir_list:
+            if os.path.isdir(os.path.join(full_path, item)):
+                ret += item + os.sep + '\n'
+            else:
+                ret += item + '\n'
+        ret = "OKAY|" + ret.strip()
+        self.send_once(ret.encode())
+
+    def cmd_mkdir(self, args):
         """
         创建目录
         :return:
         """
-        pass
+        full_path = self.check_access(args)
+        if not full_path:
+            return False
+        if os.path.isdir(full_path):
+            ret = "NO|目录已存在，不能重复创建"
+        else:
+            os.mkdir(full_path)
+            ret = "OKAY|创建完成"
+        self.send_once(ret.encode())
 
-    def rm(self, args):
+    def cmd_rm(self, args):
         """
         删除目录或者文件
         :return:
         """
         pass
 
-    def get(self, args):
+    def cmd_get(self, args):
         """
-        获取文件
+        客户端获取文件
+        :param args: 类似get "filename" from 0
         :return:
         """
-        pass
+        filename = re.search(r'.*"(.*)".*', args).group(1)  # 要获取的文件名
+        start_position = int(re.search(r'.*from (\d)$', args).group(1))    # 断点续传的起始位
+        full_path = self.check_access(filename)     # 检查是否允许访问并返回绝对路径
+        if not full_path:
+            # 如果不允许访问则返回
+            return False
+        if not os.path.isfile(full_path):
+            self.request.send("NO|文件不存在".encode())
+        else:
+            total_size = os.path.getsize(full_path) - start_position    # 要发送的总字节数
+            sent_size = 0    # 已发送的字节数
+            m = hashlib.md5()
+            self.request.send(("OKAY|" + str(total_size)).encode())     # 发送确认和即将发送的大小
+            self.request.recv(1024)     # 获取客户端的开始信号
+            with open(full_path, 'rb') as fh:
+                fh.seek(start_position)     # 根据断点定位文件
+                while total_size - sent_size > 1024:   # 当已发送的字节数小于要发送的总字节数时
+                    data = fh.read(1024)    # 读1024个字节
+                    self.request.send(data) # 发送
+                    sent_size += len(data)
+                    m.update(data)
+                else:
+                    data = fh.read(total_size - sent_size)  # 读1024个字节
+                    self.request.send(data)  # 发送
+                    sent_size += len(data)
+                    m.update(data)
+            self.request.send(m.hexdigest().encode())
 
-    def put(self, args):
+    def cmd_put(self, args):
         """
         上传文件
         :param args:

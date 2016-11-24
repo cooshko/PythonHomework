bf0c417d32ea71d3d798b53ded8b33d42b9f2be5
cooshko <coosh@qq.com>
2016-11-24 17:31:53 +0800

    堡垒机

--- day12/modules/tables.py
+++ day12/modules/tables.py
@@ -34,6 +34,16 @@ Base = declarative_base()
 session = sessionmaker(bind=engine)()
 
 
+class User2Group(Base):
+    # 用户关联组表，多对多的关系
+    __tablename__ = "user2group"
+    uid = Column(Integer, ForeignKey("user.id"), primary_key=True)
+    gid = Column(Integer, ForeignKey("user_group.id"), primary_key=True)
+    __table_args__ = (UniqueConstraint("uid", "gid", name="_u_g_uc"),)
+    user = relationship("User", backref="user2group")
+    user_group = relationship("UserGroup", backref="user2group")
+
+
 class User(Base):
     # 堡垒机用户表
     __tablename__ = "user"
@@ -41,6 +51,7 @@ class User(Base):
     name = Column(String(64), nullable=False, unique=True)
     password = Column(String(64), nullable=False)
     enable = Column(Boolean, default=True)
+    user_group = relationship("UserGroup", secondary=User2Group.__table__, backref="user")
 
 
 class UserGroup(Base):
@@ -49,14 +60,26 @@ class UserGroup(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(64), nullable=False, unique=True)
     description = Column(String(255))
+    user = relationship("User", secondary=User2Group.__table__, backref="user_group")
 
 
-class User2Group(Base):
-    # 用户关联组表，多对多的关系
-    __tablename__ = "user2group"
-    uid = Column(Integer, ForeignKey("user.id"), primary_key=True)
-    gid = Column(Integer, ForeignKey("user_group.id"), primary_key=True)
-    __table_args__ = (UniqueConstraint("uid", "gid", name="_u_g_uc"),)
+class Host2Group(Base):
+    # 主机关联主机组
+    __tablename__ = "host2group"
+    hid = Column(Integer, ForeignKey("host.id"), primary_key=True)
+    hgid = Column(Integer, ForeignKey("host_group.id"), primary_key=True)
+    __table_args__ = (UniqueConstraint("hid", "hgid", name="_h_hg_uc"),)
+    host = relationship("Host", backref="host2group")
+    host_group = relationship("HostGroup", backref="host2group")
+
+
+class Host2HostUser(Base):
+    __tablename__ = "host2host_user"
+    hid = Column(Integer, ForeignKey("host.id"), primary_key=True)
+    huid = Column(Integer, ForeignKey("host_user.id"), primary_key=True)
+    __table_args__ = (UniqueConstraint("hid", "huid", name="_h_hu_uc"),)
+    host = relationship("Host", backref="host2host_user")
+    host_user = relationship("HostUser", backref="host2host_user")
 
 
 class Host(Base):
@@ -66,6 +89,8 @@ class Host(Base):
     name = Column(String(64), nullable=False)
     ip = Column(String(15), nullable=False)
     port = Column(Integer, default=22)
+    host_user = relationship("HostUser", secondary=Host2HostUser.__table__, backref="host")
+    host_group = relationship("HostGroup", secondary=Host2Group.__table__, backref="host")
 
 
 class HostGroup(Base):
@@ -74,14 +99,22 @@ class HostGroup(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(64), nullable=False, unique=True)
     description = Column(String(255))
+    host = relationship("Host", secondary=Host2Group.__table__, backref="host_group")
 
 
-class Host2Group(Base):
-    # 主机关联主机组
-    __tablename__ = "host2group"
-    hid = Column(Integer, ForeignKey("host.id"), primary_key=True)
-    hgid = Column(Integer, ForeignKey("host_group.id"), primary_key=True)
-    __table_args__ = (UniqueConstraint("hid", "hgid", name="_h_hg_uc"),)
+class HostUser(Base):
+    # 主机上的用户
+    # 这个表不能建立唯一约束，场景是有两个组的机器都是相同的认证信息，
+    # 但后来要分开，那么在最开始的时候，应该分开来存两份一模一样的认证信息
+    __tablename__ = "host_user"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    using_key = Column(Boolean, default=False)
+    auth_user = Column(String(64), nullable=False)
+    auth_pass = Column(String(64))
+    auth_key = Column(String(255))
+    host = relationship("Host", secondary=Host2HostUser.__table__, backref="host_user")
+
+
 
 
 # class User2Host(Base):
@@ -91,24 +124,24 @@ class Host2Group(Base):
 #     hid = Column(Integer, ForeignKey("host.id"), nullable=False, primary_key=True)
 #     __table_args__ = (UniqueConstraint("hid", "uid", name="_u_h_uc"),)
 
-class AuthMethod(Base):
-    # 储存认证方法的表
-    __tablename__ = "auth_method"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    using_key = Column(Boolean, default=False)
-    auth_user = Column(String(64), nullable=False)
-    auth_pass = Column(String(64))
-    auth_key = Column(String(255))
-
-
-class UserHostAuth(Base):
-    # 用户+主机组+主机+认证方法
-    __tablename__ = "user_host_privileges"
-    uid = Column(Integer, ForeignKey("user.id"), primary_key=True)
-    hid = Column(Integer, ForeignKey("host.id"), primary_key=True)
-    hgid = Column(Integer, ForeignKey("host_group.id"))
-    mid = Column(Integer, ForeignKey("auth_method.id"), primary_key=True)
-    __table_args__ = ((UniqueConstraint("uid", "hid", "mid", name="_u_h_m_uc")),)
+# class AuthMethod(Base):
+#     # 储存认证方法的表
+#     __tablename__ = "auth_method"
+#     id = Column(Integer, primary_key=True, autoincrement=True)
+#     using_key = Column(Boolean, default=False)
+#     auth_user = Column(String(64), nullable=False)
+#     auth_pass = Column(String(64))
+#     auth_key = Column(String(255))
+#
+#
+# class UserHostAuth(Base):
+#     # 用户+主机组+主机+认证方法
+#     __tablename__ = "user_host_privileges"
+#     uid = Column(Integer, ForeignKey("user.id"), primary_key=True)
+#     hid = Column(Integer, ForeignKey("host.id"), primary_key=True)
+#     hgid = Column(Integer, ForeignKey("host_group.id"))
+#     mid = Column(Integer, ForeignKey("auth_method.id"), primary_key=True)
+#     __table_args__ = ((UniqueConstraint("uid", "hid", "mid", name="_u_h_m_uc")),)
 
 
 if __name__ == '__main__':
--- day12/modules/views.py
+++ day12/modules/views.py
@@ -4,20 +4,79 @@
 # @File    : views.py
 import sys
 from day12.modules.tables import *
+import hashlib
 
 
 class Views(object):
     @staticmethod
-    def create_user_groups(filepath):
-        with open(filepath) as fh:
-            for line in fh:
-                if line.strip():
-                    session.add(
-                        UserGroup(name=line.strip())
-                    )
+    def create_user_groups(user_groups: list):  # example: [{group1: name, desc: description}, {group2: name, desc: description}]
+        """
+        创建用户组
+        :param user_groups:
+        :return:
+        """
+        whole_list = []
+        for g in user_groups:
+            whole_list.append(UserGroup(name=g['name'], description=g['description']))
+        session.add_all(whole_list)     # 使用add_all减少数据库交换
+        session.commit()
+
+    @staticmethod
+    def create_host_groups(host_groups: list):  # example参考上面
+        """
+        创建主机组
+        :param host_groups:
+        :return:
+        """
+        whole_list = []
+        for g in host_groups:
+            whole_list.append(HostGroup(name=g['name'], description=g['description']))
+        session.add_all(whole_list)  # 使用add_all减少数据库交换
+        session.commit()
+
+    @staticmethod
+    def create_user(user_list: list):  # example: [{username1: name, password: plainText}, {username2: name, password: plainText}]
+        whole_list = []
+        for u in user_list:
+            name = u['name'].strip()
+            m = hashlib.md5()
+            m.update(u['password'])
+            passwd_md5 = m.hexdigest()
+            whole_list.append(User(name, passwd_md5))
+        session.add_all(whole_list)
         session.commit()
 
 
 if __name__ == '__main__':
-    filepath = r"../samples/new_user_groups.txt"
-    Views.create_user_groups(filepath)
+    # 创建用户组
+    # filepath = r"../samples/new_user_groups.txt"
+    # with open(filepath) as fh:
+    #     li = []
+    #     for line in fh:
+    #         try:
+    #             name, desc = line.strip().split()
+    #         except:
+    #             name = line.strip()
+    #             desc = ""
+    #         li.append({
+    #             "name": name,
+    #             "description": desc
+    #         })
+    #     Views.create_user_groups(li)
+
+    # 创建主机组
+    # filepath = r"../samples/new_host_groups.txt"
+    # with open(filepath, encoding="utf8") as fh:
+    #     li = []
+    #     for line in fh:
+    #         try:
+    #             name, desc = line.strip().split()
+    #         except:
+    #             name = line.strip()
+    #             desc = ""
+    #         li.append({
+    #             "name": name,
+    #             "description": desc
+    #         })
+    #     Views.create_host_groups(li)
+    pass
